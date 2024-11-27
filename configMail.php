c4d242a0d2ef409eff3f0a2d388e6dd003a51211
xavierfarmar <xavierfarmar@campus.monlau.com>
2024-11-27 20:14:39 +0100

    pull

--- configMail.php
+++ configMail.php
@@ -1,19 +1,29 @@
 <?php
-// configmail.php
-require_once 'mailHelper.php';
+require 'vendor/autoload.php';
 
-$email = $_POST['email']; // El email que has recibido de la petición
-$otp = rand(100000, 999999); // Generar un OTP aleatorio de 6 dígitos
+//CONFIGURACIÓN PARA EL SERVIDOR DE GMAIL
 
-$sendResult = MailHelper::sendOTP($email, $otp);
+class MailHelper {
+    public static function sendOTP($email, $otp) {
+        $mail = new PHPMailer\PHPMailer\PHPMailer();
+        $mail->isSMTP(); 
+        $mail->Host = 'smtp.gmail.com';  // Servidor SMTP de Gmail
+        $mail->SMTPAuth = true; //Esta opción autentica el usuario antes de enviar un correo. 
+        $mail->Username = '4partyapp@gmail.com'; // Mi correo de Gmail
+        $mail->Password = 'bnhf cwen ezek ihub'; // Contraseña de la aplicación de Gmail (si usas 2FA)
+        $mail->SMTPSecure = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS; //Esto asegura que la conexión sea segura mediante cifrado SSL/TLS.
+        $mail->Port = 587; // El puerto que utiliza GMAIL, para el servidor de STMP
 
-if ($sendResult['error']) {
-    echo 'Error al enviar el mensaje: ' . $sendResult['message'];
-} else {
-    echo 'Mensaje enviado con éxito';
-    // Guardar el OTP y la expiración en la base de datos
-    $expiration_time = time() + 300; // 5 minutos de expiración
-    $query = "INSERT INTO reset_otp (email, otp, expiration_time) VALUES ('$email', '$otp', '$expiration_time')";
-    mysqli_query($conn, $query);
+        // Asunto y cuerpo del mensaje
+        $mail->Subject = 'Codigo de Verificacion para restablecer contraseña';
+        $mail->Body = 'Tu codigo de verificacion es: ' . $otp . '. Este codigo expirara en 5 minutos.';
+
+        $mail->addAddress($email);
+
+        if (!$mail->send()) {
+            return ['error' => true, 'message' => $mail->ErrorInfo];
+        }
+        return ['error' => false];
+    }
 }
 ?>
---
+++ sendMail.php
@@ -0,0 +1,25 @@
+<?php
+require 'conexion.php';
+// configmail.php
+require_once 'mailHelper.php';
+
+$email = $_POST['email'];
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+ // El email que has recibido de la petición
+$otp = rand(100000, 999999); // Generar un OTP aleatorio de 6 dígitos
+
+$sendResult = MailHelper::sendOTP($email, $otp);
+
+if ($sendResult['error']) {
+    echo 'Error al enviar el mensaje: ' . $sendResult['message'];
+} else {
+    echo 'Mensaje enviado con éxito';
+    // Guardar el OTP y la expiración en la base de datos
+    $expiration_time = time() + 300; // 5 minutos de expiración
+    $query = "INSERT INTO reset_otp (email, otp, expiration_time) VALUES ('$email', '$otp', '$expiration_time')";
+    mysqli_query($conn, $query);
+}
+}
+?>
